f7cc7b7cd2ca90a7000fc9e2baceb2ede716a09c
DIvyesh <divyeshgaigol16@gmail.com>
2025-06-20 07:37:17 +0530

    Initial commit

--- app/src/main/java/com/example/healthmetricplus/data/util/Constants.kt
+++ app/src/main/java/com/example/healthmetricplus/data/util/Constants.kt
@@ -14,6 +14,6 @@ object Constants {
     const val BODY_PART_VALUE_DATE_FIELD = "date"
 
     const val GOOGLE_CLIENT_ID = "936093062231-6mg6ao186d3mul174msmmtakmemk8s2p.apps.googleusercontent.com"
-    const val APP_LOG = "MEASURE_MATE_LOG"
+    const val APP_LOG = "HEALTH_METRIC_LOG"
 
 }
